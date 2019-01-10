afa4221e503715cec319a16524ae3915edc8c574
韋智黃 <weizhihuang@weizhihuangs-MacBook-Pro.local>
2019-01-10 19:34:32 +0800

    week2 finish

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -2,9 +2,12 @@ function isPrime(n) {
     if (n === 1) {
         return false;
     }
+    if (n === 2) {
+        return true;
+    }
     for (let i = 2; i <= n / 2; i++) {
         if ((n % i) === 0) {
-            break;
+            return false;
         } else {
             return true;
         }
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,5 +1,19 @@
 function add(a, b) {
-  
+    let rtnStr = "";
+    let carryNum = 0;
+    let largerLength = Math.max(a.length, b.length);
+    for (let i = 1; i <= largerLength; i++) {
+        let aNum = a.length - i >= 0 ? Number(a[a.length - i]) : 0;
+        let bNum = b.length - i >= 0 ? Number(b[b.length - i]) : 0;
+        let sum = aNum + bNum + carryNum;
+        carryNum = sum >= 10 ? 1 : 0;
+        sum = sum >= 10 ? sum - 10 : sum;
+        rtnStr = sum.toString() + rtnStr;
+        if (i == largerLength && carryNum == 1) {
+            rtnStr = carryNum + rtnStr;
+        }
+    }
+    return rtnStr;
 }
 
 module.exports = add;
