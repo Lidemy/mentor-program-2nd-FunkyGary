1409e212ba58f3740e4df9b09d5526bb2f413724
韋智黃 <weizhihuang@weizhihuangs-MacBook-Pro.local>
2019-01-10 12:41:56 +0800

    hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,14 @@
 function isPrime(n) {
-  
+    if (n === 1) {
+        return false;
+    }
+    for (let i = 2; i <= n / 2; i++) {
+        if ((n % i) === 0) {
+            break;
+        } else {
+            return true;
+        }
+    }
 }
 
 module.exports = isPrime
